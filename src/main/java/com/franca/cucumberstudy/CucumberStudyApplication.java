5bf74f9ca8c04748bc358c8c120491fb4ec30c7a
RossyFranca <rossyfranca@gmail.com>
2023-10-30 13:39:10 -0300

    minimum configurations

---
+++ src/main/java/com/franca/cucumberstudy/CucumberStudyApplication.java
@@ -0,0 +1,13 @@
+package com.franca.cucumberstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franca/cucumberstudy/CucumberStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franca.cucumberstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
